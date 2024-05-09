ccb6be30cb759dd581dc85ee1762f398047ca8eb
Curtis Hendrickson <curtish@uab.edu>
2024-05-09 03:59:32 +0000

    VMR_MSL39_v1: add isolate_refseq, and QC vs taxonomy_node
    QC columns compare "genome_composition" and "genome_molecule" with the values in taxonomy node. Also, changes to species in last MSL are flagged, along with proposal names.
    
    In this release, SQL to update genbank and refseq accession numbers is generated by some python and shell scripts in https://github.com/ICTV-Virus-Knowledgebase/VMR_to_BlastDB, then run and saved here, instead of updating the [vmr] table from a separate lookup table [gb-rs]. We may return to that design in the future, but QC is much easier in python than in SQL.

--- VMR_load_NewMSL/8.vmr_out.sql
+++ VMR_load_NewMSL/8.vmr_out.sql
@@ -22,12 +22,21 @@ SELECT
       , CONCAT([abbrev_csv] , '') as 'Virus name abbreviation(s)'
       , CONCAT([exemplar_isolate] , [isolate_csv] , [isolate_abbrev], '') as 'Virus isolate designation'
       , CONCAT([exemplar_genbank_accession] , [isolate_genbank_accession_csv], '') as 'Virus GENBANK accession'
-      , CONCAT([exemplar_refseq_accession], '') as 'Virus REFSEQ accession'
+      , CONCAT([exemplar_refseq_accession], [isolate_refseq_accession], '') as 'Virus REFSEQ accession'
       , CONCAT([exemplar_seq_complete] , [isolate_seq_complete_csv], '') as 'Genome coverage'
       ,[molecule] as 'Genome composition'
       ,[host] as 'Host source'
-  FROM [dbo].[virus_isolates]
+	  -- QC
+	  ,vi.qc_status
+	  ,vi.qc_taxon_inher_molecule
+	  ,vi.qc_taxon_genome_coverage
+	  ,vi.qc_taxon_change
+	  ,vi.qc_taxon_proposal
+  FROM [dbo].[virus_isolates] vi
+ where left_idx is not null -- removes abolished species
   order by left_idx, sort
+
+
 
 
   
